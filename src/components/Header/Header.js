371e6149f602b7b305480a2256594590c692bd72
izarubin <izarubin@ozon.ru>
2023-03-02 09:50:01 +0300

    +

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React  from "react";
+import './Header.css';
+export const Header = ({title}) =>{
+    return(
+        <div className="header">
+           {title}
+     </div>
+    )
+}
---
+++ src/components/Tasks/Tasks.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { dividerClasses } from '@mui/material';
+
+export const Tasks = () => {
+    const [rows, setRows] = React.useState([]);
+    const getTasks = async () => {
+        const response = await fetch("http://localhost:9000/Portal/hs/content/api/tasks",{method: "GET"});
+        if (response.ok) {
+            const data = await response.json();
+            setRows(data)
+        }
+    }
+    React.useEffect(()=>{
+        getTasks();
+    },[])
+
+    return (
+
+    <div style={{ maxWidth: 500,   width: "100%"}}>
+
+    <TableContainer component={Paper}>
+      <Table sx={{ }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Код</TableCell>
+            <TableCell align="right">Наименование</TableCell>
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+
+                <TableCell align="right">
+                    {row.code}
+                </TableCell>
+
+              <TableCell align="right">
+                {row.name}
+              </TableCell>
+            
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    </div>
+  );
+}
